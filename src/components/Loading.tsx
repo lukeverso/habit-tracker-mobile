e9633cd923ebc6198688c16422ff347f89a18545
Lucas Cristovam <cristhovamlucas@gmail.com>
2023-01-27 16:04:53 -0300

    First commit

---
+++ src/components/Loading.tsx
@@ -0,0 +1,22 @@
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+
+const Loading = () => {
+     return (
+          <View style={styles.container}>
+               <ActivityIndicator
+                    color='#7C3AED'
+               />
+          </View>
+     );
+};
+
+export default Loading;
+
+const styles = StyleSheet.create({
+     container: {
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#09090A'
+     }
+});
